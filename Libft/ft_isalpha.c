c44e6317964b4b9164ce7eba91d127d743bad373
Filippo Corvaro <filippo.corvaro.fc@gmail.com>
2023-02-17 19:39:34 +0100

    adding norminette review

--- Libft/ft_isalpha.c
+++ Libft/ft_isalpha.c
@@ -6,16 +6,16 @@
 /*   By: fcorvaro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 19:04:18 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/02/17 19:33:23 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/02/17 19:38:46 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_isalpha(int	p)
-	{
-		if (str[i] >= 65 && str[i] <= 90 ||
-			str[i] >= 97 && str[i] <= 122)
-			return (1);
-		return (0);
-	}
+int	ft_isalpha(int p)
+{
+	if (str[i] >= 65 && str[i] <= 90
+		|| str[i] >= 97 && str[i] <= 122)
+		return (1);
+	return (0);
+}
