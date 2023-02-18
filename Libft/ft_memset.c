f9576821c8ea956ca2e8051940cf123088605ba1
Filippo Corvaro <filippo.corvaro.fc@gmail.com>
2023-02-18 09:45:26 +0100

    modified

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: fcorvaro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 09:11:51 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/02/18 09:43:22 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/02/18 09:44:59 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,6 @@ void	*ft_memset(void *str, int c, size_t len)
 
 /*size_t is an unsigned data type defined by standards. Is used
 to rappresent the size of an object, it must be >0. Memset 
-function sets the first count bytes of str to the value c.
+function sets the first len bytes of str to the value c.
 The value of c is converted to an unsigned char. The memset 
 function returns a pointer to str.*/
