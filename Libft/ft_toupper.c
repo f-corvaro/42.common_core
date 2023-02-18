c4deb5cfbb1b9c26b7d12b2843a9ec47fa12dbce
Filippo Corvaro <filippo.corvaro.fc@gmail.com>
2023-02-18 21:33:45 +0100

    adding ft_toupper.c

---
+++ Libft/ft_toupper.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcorvaro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/18 21:28:54 by fcorvaro          #+#    #+#             */
+/*   Updated: 2023/02/18 21:32:55 by fcorvaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_toupper(int ch)
+{
+	if (i >= 97 && i <= 122)
+		return (i - 32);
+	return (i);
+}
+
+/*toupper function converts a 
+lowercase alphabet to an uppercase alphabet.*/
