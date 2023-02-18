9cb19da8b322aa4e22244912079486befd27d6d6
Filippo Corvaro <filippo.corvaro.fc@gmail.com>
2023-02-18 11:26:14 +0100

    adding ft_memmove.c

---
+++ Libft/ft_memmove.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcorvaro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/18 10:50:56 by fcorvaro          #+#    #+#             */
+/*   Updated: 2023/02/18 11:25:39 by fcorvaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t n)
+{
+	size_t	len;
+
+	len = 0;
+	if (!dst && !src)
+		return (NULL);
+	if (src < dst)
+	{
+		len = n;
+		while (len > 0)
+		{
+			len--;
+			((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
+		}
+	}
+	else
+	{
+		len = 0;
+		while (len < n)
+		{
+			((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
+			len++;
+		}
+	}
+	return (dst);
+}
+
+/*Copies the values of num bytes from the location pointed 
+by source to the memory block pointed by destination.
+Copying takes place as if an intermediate buffer were used, 
+allowing the destination and source to overlap.*/
