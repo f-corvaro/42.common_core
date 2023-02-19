2fbe544053ca6767a3cabd12b5f29c54dc30e0a3
Filippo Corvaro <filippo.corvaro.fc@gmail.com>
2023-02-19 11:18:11 +0100

    adding ft_strdup.c

---
+++ Libft/ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcorvaro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/19 11:15:46 by fcorvaro          #+#    #+#             */
+/*   Updated: 2023/02/19 11:17:47 by fcorvaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = ft_strlen(s);
+	str = (char *)malloc(sizeof(*str) * (j + 1));
+	while (i < j)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+/*creates a dupplicate for the string passed as parameter.*/
