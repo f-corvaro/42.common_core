2cf864103b4d244bde934cac06fbc778f1f496cd
Filippo Corvaro <filippo.corvaro.fc@gmail.com>
2023-02-19 10:34:59 +0100

    adding ft_memchr.c

---
+++ Libft/ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcorvaro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/19 10:27:48 by fcorvaro          #+#    #+#             */
+/*   Updated: 2023/02/19 10:34:31 by fcorvaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*str;
+	size_t			i;
+
+	i = 0;
+	str = (unsigned char *)s;
+	while (i < n)
+	{
+		if (str[i] == (unsigned char)c)
+			return (s + i);
+		i++;
+	}
+	return (0);
+}
+
+/*scan a memory for a char*/
