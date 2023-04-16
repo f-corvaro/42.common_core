80869fe9d5ef5afa63c6db68d0acbfc98a6166cb
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-04-16 17:21:28 +0200

    ft_bzero.c

--- libft/libft/ft_bzero.c
+++ libft/libft/ft_bzero.c
@@ -6,16 +6,16 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 09:47:04 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/04/12 18:33:18 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/04/16 17:20:48 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t n)
+void	ft_bzero(void *dest, size_t len)
 {
-	ft_memset(s, 0, n);
+	ft_memset(dest, 0, len);
 }
-/*void	*ft_memset(void *str, int c, size_t len)*/
-/*Erases the data in the "n" bytes of the memory starting*/
-/*at the location pointed by "s" writing zeroes.*/
+
+/*void	*ft_memset(void *dest, int ch, size_t len)
+This function does the same thing of the memset, but ch is 0 now.*/
