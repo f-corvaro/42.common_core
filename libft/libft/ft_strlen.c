4a88bf2e9e894b8f7301425f9d82a311faa9d5fa
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-04-16 16:07:53 +0200

    ft_strlen.c

--- libft/libft/ft_strlen.c
+++ libft/libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 18:45:00 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/04/16 12:16:07 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/04/16 16:07:12 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,16 @@ int	ft_strlen(const char *str)
 	return (i);
 }
 
-/*The ft_strlen() function calculates the length of a given string*/
-/*char* is a mutable pointer to a mutable character/string.*/
-/*const char* is a mutable pointer to an immutable character/string./*
-/*You cannot change the contents of the location(s) this pointer
-/*points to.Also, compilers are required to give error messages /*
-/*when you try to do so. For the same reason, conversion from /*
-/*const char * to char* is deprecated.*/
+/*The ft_strlen() function calculates the length of a given string
+char* is a mutable pointer to a mutable character/string.
+const char* is a mutable pointer to an immutable character/string.
+You cannot change the contents of the location(s) this pointer
+points to.Also, compilers are required to give error messages
+when you try to do so. For the same reason, conversion from
+const char * to char* is deprecated.
 
-/*the code below is used to test the function
+the code below is used to test the function
+
 #include <stdio.h>
 int	main (void)
 {
