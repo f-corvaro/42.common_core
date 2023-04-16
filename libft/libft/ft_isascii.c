d290b67d87a0d0604945ca83c5ea298553c90b5b
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-04-16 11:51:46 +0200

    adding ft_isascii.c

--- libft/libft/ft_isascii.c
+++ libft/libft/ft_isascii.c
@@ -6,17 +6,30 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 19:59:06 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/03/30 15:29:25 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/04/16 11:50:50 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isascii(int p)
+int	ft_isascii(int arg)
 {
-	if (p >= 0 && p <= 127)
+	if (arg >= 0 && arg <= 127)
 		return (1);
 	return (0);
 }
 
-/*if p is an ascii return 1, or return 0 if it doesn't*/
+/*if arg is an ascii return 1, or return 0 if it doesn't*/
+
+/*the code below is used to test the function
+#include <stdio.h>
+int	main (void)
+{
+	printf("l is -> %d\n", ft_isascii('l'));
+	printf("L is -> %d\n", ft_isascii('L'));
+	printf("9 is -> %d\n", ft_isascii('9'));
+	printf("0 is -> %d\n", ft_isascii('0'));
+	printf("2 is -> %d\n", ft_isascii('2'));
+	printf("space is -> %d\n", ft_isascii(' '));
+}
+*/
