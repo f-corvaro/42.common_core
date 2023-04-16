e9a975c5268b937d730457d6a1681900f29447d0
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-04-16 16:05:58 +0200

    ft_isdigit.c

--- libft/libft/ft_isdigit.c
+++ libft/libft/ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 19:43:26 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/04/16 11:37:21 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/04/16 16:05:42 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,10 @@ int	ft_isdigit(int arg)
 		return (0);
 }
 
-/*return 1 if arg is a number or 0 if it doesn't*/
+/*return 1 if arg is a number or 0 if it doesn't
 
-/*the code below is used to test the function
+the code below is used to test the function
+
 #include <stdio.h>
 int	main (void)
 {
@@ -31,4 +32,5 @@ int	main (void)
 	printf("9 is -> %d\n", ft_isdigit('9'));
 	printf("0 is -> %d\n", ft_isdigit('0'));
 	printf("2 is -> %d\n", ft_isdigit('2'));
-}*/
+}
+*/
