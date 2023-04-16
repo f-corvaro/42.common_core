fa637cee3ac4874b92eae5bb5f441ba4c1043259
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-04-16 21:17:04 +0200

    ft_strlcpy.c

--- libft/libft/ft_strlcpy.c
+++ libft/libft/ft_strlcpy.c
@@ -6,13 +6,13 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 11:26:59 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/03/30 15:30:11 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/04/16 21:16:17 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	j;
@@ -21,17 +21,43 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	j = 0;
 	while (src[i])
 		i++;
-	if (size != 0)
+	if (size > 0)
 	{
-		while (src[i] && j < (size - 1))
+		while (j < (size - 1) && src[j])
 		{
-			dst[j] = src[j];
+			dest[j] = src[j];
 			j++;
 		}
-		dst[j] = '\0';
+		dest[j] = '\0';
 	}
 	return (i);
 }
 
-/*copy string to a specific size. The original function
-return the total lenght of the string.*/
+/*the strlcpy function is intended to replace the function strcpy (which
+copies a string to a destination buffer) with a secure version that cannot
+overflow the destination buffer. strlcpy return the total lenght of the
+string it tried to create, (lenght of src).
+
+the code below is used to test the function
+
+#include <stdio.h>
+int main ()
+{
+	char str1[15];
+	char str2[15];
+	int res;
+
+	memcpy(str1, "daje roma daje", 15);
+	memcpy(str2, "daje roma daje", 15);
+
+	res = ft_memcmp(str1, str2, 20);
+
+	if(res > 0)
+		printf("str2 is less than str1\n");
+	else if(res < 0)
+		printf("str1 is less than str2\n");
+	else
+		printf("str1 is equal to str2\n");
+	return(0);
+}
+*/
--- libft/libft/libft.h
+++ libft/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 14:37:33 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/04/16 20:29:30 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/04/16 21:15:32 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,7 @@ char	*ft_strdup(const char *s);
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 
 int		ft_strlen(const char *str);
 
