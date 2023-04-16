a3762608815cddccec4d99f830b3fd58b37fb539
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-04-16 11:59:48 +0200

    adding ft_isprint.c

--- libft/libft/ft_isprint.c
+++ libft/libft/ft_isprint.c
@@ -6,17 +6,30 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 21:17:20 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/03/30 15:29:33 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/04/16 11:59:11 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isprint(int p)
+int	ft_isprint(int arg)
 {
-	if (p >= 32 && p <= 126)
+	if (arg >= 32 && arg <= 127)
 		return (1);
-	return (0);
+	else
+		return (0);
 }
 
-/*it return 1 if is printable character or 0 if it doesn't*/
+/*the function return 1 if arg is a printable character or 0 if it doesn't*/
+
+/*the code below is used to test the function
+#include <stdio.h>
+int	main (void)
+{
+	printf("l is -> %d\n", ft_isprint('l'));
+	printf("L is -> %d\n", ft_isprint('L'));
+	printf("9 is -> %d\n", ft_isprint('9'));
+	printf("0 is -> %d\n", ft_isprint('0'));
+	printf("2 is -> %d\n", ft_isprint('2'));
+	printf("space is -> %d\n", ft_isprint(' '));
+}*/
