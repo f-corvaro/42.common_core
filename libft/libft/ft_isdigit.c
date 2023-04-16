c9c6aea630a80ff0115c3e65b692c7c29f7e0ba0
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-04-16 11:38:05 +0200

    adding ft_isdigit.c

--- libft/libft/ft_isdigit.c
+++ libft/libft/ft_isdigit.c
@@ -6,17 +6,29 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 19:43:26 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/03/30 15:29:29 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/04/16 11:37:21 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isdigit(int p)
+int	ft_isdigit(int arg)
 {
-	if (p >= 48 && p <= 57)
+	if (arg >= '0' && arg <= '9')
 		return (1);
-	return (0);
+	else
+		return (0);
 }
 
-/*return 1 if p is a number or 0 if it doesn't*/
+/*return 1 if arg is a number or 0 if it doesn't*/
+
+/*the code below is used to test the function
+#include <stdio.h>
+int	main (void)
+{
+	printf("l is -> %d\n", ft_isdigit('l'));
+	printf("L is -> %d\n", ft_isdigit('L'));
+	printf("9 is -> %d\n", ft_isdigit('9'));
+	printf("0 is -> %d\n", ft_isdigit('0'));
+	printf("2 is -> %d\n", ft_isdigit('2'));
+}*/
