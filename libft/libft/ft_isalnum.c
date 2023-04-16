2990ade28471ecf5b8b4130c569118fda30f34f4
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-04-16 11:46:30 +0200

    adding ft_isalnum.c

--- libft/libft/ft_isalnum.c
+++ libft/libft/ft_isalnum.c
@@ -6,19 +6,33 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 19:50:34 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/03/30 15:59:11 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/04/16 11:45:45 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalnum(int p)
+int	ft_isalnum(int arg)
 {
-	if ((p >= 65 && p <= 90)
-		|| (p >= 97 && p <= 122)
-		|| (p >= 48 && p <= 57))
+	if (((arg >= '0' && arg <= '9')
+			|| (arg >= 'a' && arg <= 'z'))
+		|| (arg >= 'A' && arg <= 'Z'))
 		return (1);
-	return (0);
+	else
+		return (0);
 }
 
-/*return 1 if p is alphanumeric or 0 if it doesn't*/
+/*return 1 if arg is alphanumeric or 0 if it doesn't*/
+
+/*the code below is used to test the function
+#include <stdio.h>
+int	main (void)
+{
+	printf("l is -> %d\n", ft_isalnum('l'));
+	printf("L is -> %d\n", ft_isalnum('L'));
+	printf("9 is -> %d\n", ft_isalnum('9'));
+	printf("0 is -> %d\n", ft_isalnum('0'));
+	printf("2 is -> %d\n", ft_isalnum('2'));
+	printf("space is -> %d\n", ft_isalnum(' '));
+}
+*/
