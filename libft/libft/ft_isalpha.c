fc34f878532dc053bdfaaf1b5d70aa464b426059
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-04-16 16:04:19 +0200

    adding ft_isalpha.c

--- libft/libft/ft_isalpha.c
+++ libft/libft/ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 19:04:18 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/04/16 11:23:58 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/04/16 16:03:54 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,11 @@ int	ft_isalpha(int arg)
 		return (0);
 }
 
-/*If the parameter isn't an alphabet will return 0, otherwise, if is char*/
-/*will return 1*/
+/*If the parameter isn't an alphabet will return 0, otherwise, if is char
+will return 1
 
-/*the code below is used to test the function
+the code below is used to test the function
+
 #include <stdio.h>
 int	main (void)
 {
