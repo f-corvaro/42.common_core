54f85174827f963bf6f639f18c8c4cd1c8365583
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-04-16 11:10:11 +0200

    adding ft_isalpha.c

--- libft/libft/ft_isalpha.c
+++ libft/libft/ft_isalpha.c
@@ -6,18 +6,19 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 19:04:18 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/03/30 15:59:37 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/04/16 11:09:29 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalpha(int p)
+int	ft_isalpha(int c)
 {
-	if ((p >= 65 && p <= 90)
-		|| (p >= 97 && p <= 122))
+	if ((c >= 'a' && c <= 'z')
+		|| (c >= 'A' && c <= 'Z'))
 		return (1);
-	return (0);
+	else
+		return (0);
 }
-
-/*return 1 if p is lowercase or uppercase or 0 if it doesn't*/
+/*If the parameter isn't an alphabet will return 0, otherwise, if is char*/
+/*will return 1*/
