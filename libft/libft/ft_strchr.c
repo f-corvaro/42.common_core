62e4fadfec8bb001c0b3e24947fcfb0642bda94c
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-04-16 20:29:43 +0200

    ft_strchr.c

--- libft/libft/ft_strchr.c
+++ libft/libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 09:54:33 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/03/30 15:29:59 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/04/16 20:28:58 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,39 @@
 
 char	*ft_strchr(const char *str, int c)
 {
-	while (*str)
+	int	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
 	{
-		if (*str == c)
-			return ((char *)str);
-		str++;
+		if (str[i] == (char)c)
+			return ((char *)&str[i]);
+		i++;
 	}
-	if (c == '\0')
-		return ((char *)str);
+	if (str[i] == (char)c)
+		return ((char *)&str[i]);
 	return (0);
 }
 
-/*This returns a pointer to the first occurrence of the character 
-c in the string str, or NULL if the character is not found.*/
+
+/*This returns a pointer to the first occurrence of the character
+c in the string str, or NULL if the character is not found.
+
+the code below is used to test the function
+
+#include <stdio.h>
+int main ()
+{
+	const char str[] = "daje roma daje";
+	const char ch = 'm';
+	char *res;
+
+	res = ft_strchr(str, ch);
+
+	printf("String after %c is -> %s\n", ch, res);
+
+	return(0);
+}
+*/
