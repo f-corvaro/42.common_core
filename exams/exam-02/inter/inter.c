182861dcb1403c7856337f49818aeda07f90e5d4
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-05-16 13:24:06 +0200

    adding inter.c file

---
+++ exams/exam-02/inter/inter.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+
+int	check(char *s, int i, int p)
+{
+	int	k;
+
+	k = 0;
+	while (k < p)
+	{
+		if (s[k] == i)
+			return (0);
+		k++;
+	}
+	return (1);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+	
+	if (ac == 3)
+	{
+		i = 0;
+		while (av[1][i])
+		{
+			j = 0;
+			while (av[2][j])
+			{
+				if (av[1][i] == av[2][j])
+				{
+					if (check(av[1], av[1][i], i))
+					{
+						write (1, &av[1][i], 1);
+						break;
+					}
+				}
+				j++;
+			}
+			i++;
+		}
+	}
+	write (1, "\n", 1);
+}
