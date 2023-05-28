201608c86edcae8102e7d9fcc5b53d11a4e8da7e
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-05-28 15:59:32 +0200

    gnl.c adding

---
+++ exams/exam-02/gnl/get_next_line.c
@@ -0,0 +1,26 @@
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	int	i;
+	int	rd;
+	char	c;
+	char	*buffer;
+
+	i = 0;
+	rd = 0;
+	*buffer = malloc(100000);
+
+	if (BUFFER_SIZE <= 0)
+		return (NULL);
+	while ((rd = read(fd, &c, BUFFER_SIZE - BUFFER_SIZE + 1)) > 0)
+	{
+		buffer[i++] = c;
+		if (c == '\n')
+			break;
+	}
+	buffer[i] = '\0';
+	if (rd == -1 || i == 0 || (!buffer[i - 1] && !rd))
+		return (free(buffer), NULL);
+	return(buffer);
+}
