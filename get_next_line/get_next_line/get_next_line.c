d622b350255ec51af85868eaf59ffab860dd19aa
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-09-19 11:57:15 +0200

    MANDATORY PART

--- get_next_line/get_next_line/get_next_line.c
+++ get_next_line/get_next_line/get_next_line.c
@@ -6,7 +6,58 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 19:54:45 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/09/15 19:54:46 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/09/19 11:56:27 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	ft_len(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+/*This function returns the lenght of the str. Used to discover the lenght of
+the line to read*/
+
+char	*get_next_line(int fd)
+{
+	char		*out;
+	static char	*str;
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	str = ft_line_allocation(fd, str);
+	if (!str)
+		return (NULL);
+	out = ft_next_line(str);
+	str = ft_rem_line(str);
+	return (out);
+}
+/* This function reads a line of text from a file descriptor and returns a new
+string containing the line of text.*/
+/*
+int	main(void)
+{
+	char		*s;
+	int		i;
+	int		fd;
+
+	fd = open("file.txt", O_RDONLY);
+	i = 1;
+	while (i < 15)
+	{
+		s = get_next_line(fd);
+		printf("line [%02d]: %s", i, s);
+		free(s);
+		i++;
+	}
+	close(fd);
+	return (0);
+}*/
--- get_next_line/get_next_line/get_next_line.h
+++ get_next_line/get_next_line/get_next_line.h
@@ -6,7 +6,38 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 19:55:04 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/09/15 19:55:05 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/09/19 11:35:13 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stddef.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+size_t	ft_len(char *str);
+char	*ft_strchr(char *str, int c);
+char	*ft_strjoin(char *str1, char *str2);
+char	*ft_line_allocation(int fd, char *str);
+char	*ft_next_line(char *str);
+char	*ft_rem_line(char *line);
+char	*get_next_line(int fd);
+#endif
+
+/*
+1 -> for printf, BUFFER_SIZE
+2 -> for size_t, sizeof()
+3 -> for open(), read()
+4 -> for malloc(), free()
+5 -> for ssize_t
+6 -> for O_RDONLY
+*/
