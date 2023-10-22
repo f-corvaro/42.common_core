6eed1ba59a0d55c2c6c2cdc4a0ac269bd92ab317
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-10-22 19:12:25 +0200

    adding functions

---
+++ ft_printf/ft_printf/ft_printf_utils.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/22 19:04:14 by fcorvaro          #+#    #+#             */
+/*   Updated: 2023/10/22 19:11:47 by fcorvaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+
+/*functions;*/
+
+#endif
