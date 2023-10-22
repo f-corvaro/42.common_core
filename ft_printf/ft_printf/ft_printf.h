bf705e25f631dee986a4d89ecaf081b0caac3e1a
f-corvaro <filippo.corvaro.fc@gmail.com>
2023-10-22 19:13:34 +0200

    adding functions

--- ft_printf/ft_printf/ft_printf.h
+++ ft_printf/ft_printf/ft_printf.h
@@ -6,7 +6,17 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 19:03:58 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/10/22 19:03:59 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/10/22 19:12:54 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+
+/*functions;*/
+
+#endif
--- ft_printf/ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf/ft_printf_utils.c
@@ -6,17 +6,8 @@
 /*   By: fcorvaro <fcorvaro@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 19:04:14 by fcorvaro          #+#    #+#             */
-/*   Updated: 2023/10/22 19:11:47 by fcorvaro         ###   ########.fr       */
+/*   Updated: 2023/10/22 19:13:22 by fcorvaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-
-# include <stdarg.h>
-# include <unistd.h>
-# include <stdio.h>
-
-/*functions;*/
-
-#endif
+#include "ft_printf.h"
