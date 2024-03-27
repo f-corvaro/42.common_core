bd6c792a7dc1319d934394e15e2858839bac991c
fcorvaro <filippo.corvaro.fc@gmail.com>
2024-03-27 12:49:07 +0100

    adding my ft_printf

--- exams/exam-03/ft_printf/ft_printf.c
+++ exams/exam-03/ft_printf/ft_printf.c
@@ -1,45 +1,33 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: f-corvaro <f-corvaro@student.42.fr>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/23 13:14:09 by f-corvaro         #+#    #+#             */
-/*   Updated: 2024/03/23 13:23:10 by f-corvaro        ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 #include <stdarg.h>
 
-void	put_string(char *string, int *length)
+void	put_str(char *str, int *len)
 {
-	if (!string)
-		string = "(null)";
-	while (*string)
-		*length += write(1, string++, 1);
+	if (!str)
+		str = "(null)";
+	while (*str)
+		*len += write(1, str++, 1);
 }
 
-void	put_digit(long long int number, int base, int *length)
+void	put_digit(long long int nb, int base, int *len)
 {
-	char	*hexadecimal = "0123456789abcdef";
+	char	*hex = "0123456789abcdef";
 
-	if (number < 0)
+	if (nb < 0)
 	{
-		number *= -1;
-		*length += write(1, "-", 1);
+		nb *= -1;
+		*len += write(1, "-", 1);
 	}
-	if (number >= base)
-		put_digit((number / base), base, length);
-	*length += write(1, &hexadecimal[number % base], 1);
+	if (nb >= base)
+		put_digit((nb / base), base, len);
+	*len += write(1, &hex[nb % base], 1);
 }
 
 int	ft_printf(const char *format, ...)
 {
-	int length = 0;
+	int	len = 0;
 
-	va_list	pointer;
+	va_list pointer;
 	va_start(pointer, format);
 
 	while (*format)
@@ -48,15 +36,15 @@ int	ft_printf(const char *format, ...)
 		{
 			format++;
 			if (*format == 's')
-				put_string(va_arg(pointer, char *), &length);
+				put_str(va_arg(pointer, char *), &len);
 			else if (*format == 'd')
-				put_digit((long long int)va_arg(pointer, int), 10, &length);
+				put_digit((long long int)va_arg(pointer, int), 10, &len);
 			else if (*format == 'x')
-				put_digit((long long int)va_arg(pointer, unsigned int), 16, &length);
+				put_digit((long long int)va_arg(pointer, unsigned int), 16, &len);
 		}
 		else
-			length += write(1, format, 1);
+			len += write(1, format, 1);
 		format++;
 	}
-	return (va_end(pointer), length);
+	return (va_end(pointer), len);
 }
