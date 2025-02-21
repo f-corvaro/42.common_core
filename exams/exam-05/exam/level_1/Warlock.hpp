4b7ce86135a96292257cad2ab53f8045fd3d3ac7
fcorvaro <filippo.corvaro.fc@gmail.com>
2025-02-21 12:34:17 +0100

    Updating solutions

--- exams/exam-05/exam/level_1/Warlock.hpp
+++ exams/exam-05/exam/level_1/Warlock.hpp
@@ -1,18 +1,18 @@
 #include <iostream>
 
 class Warlock {
-private:
-	std::string _name;
-	std::string _title;
-	Warlock();
-	Warlock(Warlock const & obj);
-	Warlock & operator=(Warlock const & rhs);
-public:
-	Warlock(const std::string& name, const std::string& title);
-	~Warlock();
+	private:
+		std::string _name;
+		std::string _title;
+		Warlock();
+		Warlock(Warlock const & obj);
+		Warlock & operator=(Warlock const & rhs);
+	public:
+		Warlock(const std::string& name, const std::string& title);
+		~Warlock();
 
-	std::string const & getName() const;
-	std::string const & getTitle() const;
-	void	setTitle(std::string const & str);
-	void	introduce() const;
+		std::string const & getName() const;
+		std::string const & getTitle() const;
+		void	setTitle(std::string const & str);
+		void	introduce() const;
 };
